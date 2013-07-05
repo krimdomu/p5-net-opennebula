ad7f16bd0810e99bf657312876bc0ba9ab339c52
Jan <krimdomu@googlemail.com>
2013-07-06 00:38:48 +0200

    inital commit

---
+++ lib/Net/OpenNebula.pm
@@ -0,0 +1,220 @@
+#
+# (c) Jan Gehring <jan.gehring@gmail.com>
+# 
+# vim: set ts=3 sw=3 tw=0:
+# vim: set expandtab:
+# 
+
+=head1 NAME
+
+Net::OpenNebula - Access OpenNebula RPC via Perl.
+
+=head1 DESCRIPTION
+
+With this module you can access the OpenNebula XML-RPC service.
+
+=head1 SYNOPSIS
+
+ use Net::OpenNebula;
+ my $one = Net::OpenNebula->new(
+    url      => "http://server:2633/RPC2",
+    user     => "oneadmin",
+    password => "onepass",
+ );
+    
+ my @vms = $one->get_vms();
+
+=cut
+
+package Net::OpenNebula;
+
+use strict;
+use warnings;
+
+use XML::Simple;
+use RPC::XML;
+use RPC::XML::Client;
+
+use Data::Dumper;
+
+use Net::OpenNebula::Host;
+use Net::OpenNebula::Cluster;
+use Net::OpenNebula::VM;
+use Net::OpenNebula::Template;
+
+our $VERSION = "0.0.1";
+
+sub new {
+   my $that = shift;
+   my $proto = ref($that) || $that;
+   my $self = { @_ };
+
+   bless($self, $proto);
+
+   return $self;
+}
+
+sub get_clusters {
+   my ($self) = @_;
+
+   my @ret = ();
+
+   my $data = $self->_rpc("one.clusterpool.info");
+
+   for my $cluster (@{ $data->{CLUSTER} }) {
+      push(@ret, Net::OpenNebula::Cluster->new(rpc => $self, data => $cluster));
+   }
+
+   return @ret;
+}
+
+sub get_hosts {
+   my ($self) = @_;
+
+   my @ret = ();
+
+   my $data = $self->_rpc("one.hostpool.info");
+
+   for my $host (@{ $data->{HOST} }) {
+      push(@ret, Net::OpenNebula::Host->new(rpc => $self, data => $host));
+   }
+
+   return @ret;
+}
+
+sub get_host {
+   my ($self, $id) = @_;
+
+   if(! defined $id) {
+      die("You have to define the ID => Usage: \$obj->get_host(\$host_id)");
+   }
+
+   my $data = $self->_rpc("one.host.info", [ int => $id ]);
+   return Net::OpenNebula::Host->new(rpc => $self, data => $data, extended_data => $data);
+}
+
+sub get_vms {
+   my ($self) = @_;
+
+   my $data = $self->_rpc("one.vmpool.info", 
+                           [ int => -2 ], # always get all resources
+                           [ int => -1 ], # range from (begin)
+                           [ int => -1 ], # range to (end)
+                           [ int => -1 ], # all states, except DONE
+                         ); 
+
+   my @ret = ();
+
+   for my $vm (@{ $data->{VM} }) {
+      push(@ret, Net::OpenNebula::VM->new(rpc => $self, data => $vm));
+   }
+
+   return @ret;
+}
+
+sub get_vm {
+   my ($self, $id) = @_;
+
+   if(! defined $id) {
+      die("You have to define the ID => Usage: \$obj->get_vm(\$vm_id)");
+   }
+
+
+   if($id =~ m/^\d+$/) {
+      my $data = $self->_rpc("one.vm.info", [ int => $id ]);
+      return Net::OpenNebula::VM->new(rpc => $self, data => $data, extended_data => $data);
+   }
+   else {
+      # try to find vm by name
+      my ($vm) = grep { $_->name eq $id } $self->get_vms;
+      return $vm;
+   }
+
+}
+
+sub get_templates {
+   my ($self) = @_;
+
+   my $data = $self->_rpc("one.templatepool.info",
+                           [ int => -2 ], # all templates
+                           [ int => -1 ], # range start
+                           [ int => -1 ], # range end
+                         );
+
+   my @ret = ();
+
+   for my $tpl (@{ $data->{VMTEMPLATE} } ) {
+      push(@ret, Net::OpenNebula::Template->new(rpc => $self, data => $tpl));
+   }
+
+   return @ret;
+}
+
+sub create_vm {
+   my ($self, %option) = @_;
+
+   my $template;
+
+   if($option{template} =~ m/^\d+$/) {
+      ($template) = grep { $_->id == $option{template} } $self->get_templates;   
+   }
+   else {
+      ($template) = grep { $_->name eq $option{template} } $self->get_templates;   
+   }
+
+   my $hash_ref = $template->get_template_ref;
+   $hash_ref->{TEMPLATE}->[0]->{NAME}->[0] = $option{name};
+
+   my $s = XMLout($hash_ref, RootName => undef, NoIndent => 1 );
+
+   my $res = $self->_rpc("one.vm.allocate", [ string => $s ]);
+
+   return $self->get_vm($res);
+}
+
+sub create_host {
+   my ($self, %option) = @_;
+
+   my $data = $self->_rpc("one.host.allocate",
+                              [ string => $option{name} ],
+                              [ string => $option{im_mad} ],
+                              [ string => $option{vmm_mad} ],
+                              [ string => $option{vnm_mad} ],
+                              [ int => (exists $option{cluster} ? $option{cluster} : -1) ] );
+   if(ref($data) ne "ARRAY") {
+      return;
+   }
+
+   return $self->get_host($data->[1]);
+}
+
+sub _rpc {
+   my ($self, $meth, @params) = @_;                                                                                
+
+   my @params_o = (RPC::XML::string->new($self->{user} . ":" . $self->{password}));
+   for my $p (@params) {
+      my $klass = "RPC::XML::" . $p->[0];
+      push(@params_o, $klass->new($p->[1]));
+   }   
+
+   my $req = RPC::XML::request->new($meth, @params_o);
+   my $cli = RPC::XML::Client->new($self->{url});
+   my $resp = $cli->send_request($req);
+   my $ret = $resp->value;
+
+   if($ret->[0] == 1) {
+      if($ret->[1] =~ m/^\d+$/) {
+         return $ret->[1];
+      }
+      else {
+         return XMLin($ret->[1], ForceArray => 1);
+      }
+   }   
+
+   else {
+      die("error sending request.");
+   }   
+
+}
+
+1;
---
+++ lib/Net/OpenNebula/VM.pm
@@ -0,0 +1,161 @@
+#
+# (c) Jan Gehring <jan.gehring@gmail.com>
+# 
+# vim: set ts=3 sw=3 tw=0:
+# vim: set expandtab:
+#
+# !no_doc!
+   
+
+package Net::OpenNebula::VM;
+
+use strict;
+use warnings;
+
+use Net::OpenNebula::VM::NIC;
+
+use Data::Dumper;
+
+sub new {
+   my $that = shift;
+   my $proto = ref($that) || $that;
+   my $self = { @_ };
+
+   bless($self, $proto);
+
+   return $self;
+}
+
+sub id {
+   my ($self) = @_;
+   return $self->{data}->{ID}->[0];
+}
+
+sub name {
+   my ($self) = @_;
+   $self->_get_info();
+
+   return $self->{extended_data}->{TEMPLATE}->[0]->{NAME}->[0];
+}
+
+sub nics {
+   my ($self) = @_;
+   $self->_get_info();
+
+   my @ret = ();
+
+   for my $nic (@{ $self->{extended_data}->{TEMPLATE}->[0]->{NIC} }) {
+      push(@ret, Net::OpenNebula::VM::NIC->new(data => $nic));
+   }
+
+   return @ret;
+}
+
+sub shutdown {
+   my ($self) = @_;
+   $self->{rpc}->_rpc("one.vm.action",
+                        [ string => "shutdown" ],
+                        [ int => $self->id ],
+                     );
+}
+
+sub reboot {
+   my ($self) = @_;
+   $self->{rpc}->_rpc("one.vm.action",
+                        [ string => "reboot" ],
+                        [ int => $self->id ],
+                     );
+}
+
+sub suspend {
+   my ($self) = @_;
+   $self->{rpc}->_rpc("one.vm.action",
+                        [ string => "suspend" ],
+                        [ int => $self->id ],
+                     );
+}
+
+sub resume {
+   my ($self) = @_;
+   $self->{rpc}->_rpc("one.vm.action",
+                        [ string => "resume" ],
+                        [ int => $self->id ],
+                     );
+}
+
+sub restart {
+   my ($self) = @_;
+   $self->{rpc}->_rpc("one.vm.action",
+                        [ string => "restart" ],
+                        [ int => $self->id ],
+                     );
+}
+
+sub stop {
+   my ($self) = @_;
+   $self->{rpc}->_rpc("one.vm.action",
+                        [ string => "stop" ],
+                        [ int => $self->id ],
+                     );
+}
+
+sub start {
+   my ($self) = @_;
+   $self->{rpc}->_rpc("one.vm.action",
+                        [ string => "start" ],
+                        [ int => $self->id ],
+                     );
+}
+
+# don't know how to get the state properly. didn't found good docs.
+sub state {
+   my ($self) = @_;
+   $self->_get_info(clearcache => 1);
+
+   if($self->{extended_data}->{STATE}->[0] == 1) {
+      return "pending";
+   }
+
+   if($self->{extended_data}->{STATE}->[0] == 3 
+      && $self->{extended_data}->{LAST_POLL}->[0] == 0) {
+      return "prolog";
+   }
+
+   if($self->{extended_data}->{STATE}->[0] == 3
+      && $self->{extended_data}->{LAST_POLL}->[0]
+      && $self->{extended_data}->{LAST_POLL}->[0] > 0) {
+      return "running";
+   }
+
+   if($self->{extended_data}->{LCM_STATE}->[0] == 12) {
+      return "shutdown";
+   }
+
+   if($self->{extended_data}->{LCM_STATE}->[0] == 0
+      && $self->{extended_data}->{LCM_STATE}->[0] == 6) {
+      return "done";
+   }
+}
+
+sub arch {
+   my ($self) = @_;
+   $self->_get_info;
+
+   return $self->{extended_data}->{TEMPLATE}->[0]->{OS}->[0]->{ARCH}->[0];
+}
+
+sub _get_info {
+   my ($self, %option) = @_;
+
+   if(! exists $self->{extended_data} || (exists $option{clearcache} && $option{clearcache} == 1)) {
+      $self->{extended_data} = $self->{rpc}->_rpc("one.vm.info", [ int => $self->id ]);
+   }
+}
+
+sub get_data {
+   my ($self) = @_;
+   $self->_get_info;
+   return $self->{extended_data};
+}
+
+1;
