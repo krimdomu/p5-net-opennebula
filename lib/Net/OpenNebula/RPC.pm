04d94b1d73da908c69c1f6f17cc7d1f9e71f6307
stdweird <stijn.deweirdt@ugent.be>
2014-07-14 13:54:43 +0200

    introduce _onerpc and _onerpcsimple

--- lib/Net/OpenNebula/RPC.pm
+++ lib/Net/OpenNebula/RPC.pm
@@ -5,15 +5,32 @@ use warnings;
 
 use Data::Dumper;
 
+use constant ONERPC => 'rpc';
+
 sub new {
    my $that = shift;
    my $proto = ref($that) || $that;
    my $self = { @_ };
 
+   $self->{ONERPC} = $proto->ONERPC; 
+   
    bless($self, $proto);
 
    return $self;
 }
 
+sub _onerpc {
+    my ($self, $method, @args) = @_;
+
+    return $self->{rpc}->_rpc("one.$self->{ONERPC}.$method", @args);
+}
+
+sub _onerpc_simple {
+    my ($self, $method, $arg) = @_;
+    return $self->_onerpc($method,
+                            [ string => "$arg" ],
+                            [ int => $self->id ],
+                         );
+};
 
 1;
