3fa4225041a74e16858c38535951b1322967dc24
stdweird <stijn.deweirdt@ugent.be>
2014-07-14 22:01:31 +0200

    add Image support

---
+++ lib/Net/OpenNebula/Image.pm
@@ -0,0 +1,39 @@
+#
+# (c) Jan Gehring <jan.gehring@gmail.com>
+# 
+# vim: set ts=3 sw=3 tw=0:
+# vim: set expandtab:
+#
+   
+
+package Net::OpenNebula::Image;
+
+use strict;
+use warnings;
+
+use Net::OpenNebula::RPC;
+push our @ISA , qw(Net::OpenNebula::RPC);
+
+use constant ONERPC => 'image';
+
+sub name {
+   my ($self) = @_;
+   $self->_get_info();
+
+   return $self->{extended_data}->{NAME}->[0];
+}
+
+sub create {
+   my ($self, $tpl_txt) = @_;
+   my $id = $self->_onerpc("allocate", [ string => $tpl_txt ]);
+   $self->{data} =  $self->_get_info(id => $id); 
+   return $id;
+}
+
+
+sub delete {
+    my ($self) = @_;
+    return $self->_onerpc_id("delete");
+}
+
+1;
