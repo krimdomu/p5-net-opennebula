c792f49ea79e8d0255d11c44ef41dd5246ed39ad
stdweird <stijn.deweirdt@ugent.be>
2014-07-14 14:06:37 +0200

    rewite VM.pm with operpc and onerpc simple, including factory for most actions

--- lib/Net/OpenNebula/VM.pm
+++ lib/Net/OpenNebula/VM.pm
@@ -12,9 +12,10 @@ package Net::OpenNebula::VM;
 use strict;
 use warnings;
 
-use Net::Opennebula::RPC;
+use Net::OpenNebula::RPC;
+push our @ISA , qw(Net::OpenNebula::RPC);
 
-@ISA = ("Net::Opennebula::RPC");
+use constant ONERPC => 'vm';
 
 use Net::OpenNebula::VM::NIC;
 
@@ -45,174 +46,18 @@ sub nics {
    return @ret;
 }
 
-sub shutdown {
-   my ($self) = @_;
-   $self->{rpc}->_rpc("one.vm.action",
-                        [ string => "shutdown" ],
-                        [ int => $self->id ],
-                     );
-}
-
-sub reboot {
-   my ($self) = @_;
-   $self->{rpc}->_rpc("one.vm.action",
-                        [ string => "reboot" ],
-                        [ int => $self->id ],
-                     );
-}
-
-sub reboot_hard {
-   my ($self) = @_;
-   $self->{rpc}->_rpc("one.vm.action",
-                        [ string => "reboot-hard" ],
-                        [ int => $self->id ],
-                     );
-}
-
-sub poweroff {
-   my ($self) = @_;
-   $self->{rpc}->_rpc("one.vm.action",
-                        [ string => "poweroff" ],
-                        [ int => $self->id ],
-                     );
-}
-
-sub poweroff_hard {
-   my ($self) = @_;
-   $self->{rpc}->_rpc("one.vm.action",
-                        [ string => "poweroff-hard" ],
-                        [ int => $self->id ],
-                     );
-}
-
-sub suspend {
-   my ($self) = @_;
-   $self->{rpc}->_rpc("one.vm.action",
-                        [ string => "suspend" ],
-                        [ int => $self->id ],
-                     );
-}
-
-sub resume {
-   my ($self) = @_;
-   $self->{rpc}->_rpc("one.vm.action",
-                        [ string => "resume" ],
-                        [ int => $self->id ],
-                     );
-}
-
-sub restart {
-   my ($self) = @_;
-   $self->{rpc}->_rpc("one.vm.action",
-                        [ string => "restart" ],
-                        [ int => $self->id ],
-                     );
-}
-
-sub stop {
-   my ($self) = @_;
-   $self->{rpc}->_rpc("one.vm.action",
-                        [ string => "stop" ],
-                        [ int => $self->id ],
-                     );
-}
 
 sub start {
    my ($self) = @_;
    $self->_get_info(clearcache => 1);
 
    if($self->{extended_data}->{STATE}->[0] == 5 || $self->{extended_data}->{STATE}->[0] == 4 || $self->{extended_data}->{STATE}->[0] == 8) {
-      $self->resume();
+      return $self->resume();
    }
    else {
-      $self->{rpc}->_rpc("one.vm.action",
-                           [ string => "start" ],
-                           [ int => $self->id ],
-                        );
+      return $self->_onerpcsimple("action", "start");
    }
 }
-
-sub delete {
-   my ($self) = @_;
-   $self->{rpc}->_rpc("one.vm.action",
-                        [ string => "delete" ],
-                        [ int => $self->id ],
-                     );
-}
-
-sub delete_recreate {
-   my ($self) = @_;
-   $self->{rpc}->_rpc("one.vm.action",
-                        [ string => "delete-recreate" ],
-                        [ int => $self->id ],
-                     );
-}
-
-sub shutdown_hard {
-   my ($self) = @_;
-   $self->{rpc}->_rpc("one.vm.action",
-                        [ string => "shutdown-hard" ],
-                        [ int => $self->id ],
-                     );
-}
-
-sub hold {
-   my ($self) = @_;
-   $self->{rpc}->_rpc("one.vm.action",
-                        [ string => "hold" ],
-                        [ int => $self->id ],
-                     );
-}
-
-sub release {
-   my ($self) = @_;
-   $self->{rpc}->_rpc("one.vm.action",
-                        [ string => "release" ],
-                        [ int => $self->id ],
-                     );
-}
-
-sub boot {
-   my ($self) = @_;
-   $self->{rpc}->_rpc("one.vm.action",
-                        [ string => "boot" ],
-                        [ int => $self->id ],
-                     );
-}
-
-sub resched {
-   my ($self) = @_;
-   $self->{rpc}->_rpc("one.vm.action",
-                        [ string => "resched" ],
-                        [ int => $self->id ],
-                     );
-}
-
-sub unresched {
-   my ($self) = @_;
-   $self->{rpc}->_rpc("one.vm.action",
-                        [ string => "unresched" ],
-                        [ int => $self->id ],
-                     );
-}
-
-sub undeploy {
-   my ($self) = @_;
-   $self->{rpc}->_rpc("one.vm.action",
-                        [ string => "undeploy" ],
-                        [ int => $self->id ],
-                     );
-}
-
-sub undeploy_hard {
-   my ($self) = @_;
-   $self->{rpc}->_rpc("one.vm.action",
-                        [ string => "undeploy-hard" ],
-                        [ int => $self->id ],
-                     );
-}
-
-
 
 # don't know how to get the state properly. didn't found good docs.
 sub state {
@@ -261,7 +106,7 @@ sub _get_info {
    my ($self, %option) = @_;
 
    if(! exists $self->{extended_data} || (exists $option{clearcache} && $option{clearcache} == 1)) {
-      $self->{extended_data} = $self->{rpc}->_rpc("one.vm.info", [ int => $self->id ]);
+      $self->{extended_data} = $self->_onerpc("info", [ int => $self->id ]);
    }
 }
 
@@ -270,5 +115,17 @@ sub get_data {
    $self->_get_info;
    return $self->{extended_data};
 }
+
+# define all generic actions
+no strict 'refs';
+foreach my $i (qw(shutdown shutdown_hard reboot reboot_hard poweroff poweroff_hard 
+                  suspend resume restart stop delete delete_recreate hold release 
+                  boot resched unresched undeploy undeploy_hard)) {
+    *{$i} = sub {
+        my $self = shift;
+        return $self->_onerpcsimple("action", $i);
+    }
+}
+use strict 'refs';
 
 1;
