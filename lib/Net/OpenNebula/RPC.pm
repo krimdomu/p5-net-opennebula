49162340bde39dc1f24f06e174fe223a77ef8fa6
stdweird <stijn.deweirdt@ugent.be>
2014-07-14 13:17:50 +0200

    make a common rpc module, all others can/will inherit from this one

---
+++ lib/Net/OpenNebula/RPC.pm
@@ -0,0 +1,19 @@
+package Net::OpenNebula::RPC;
+
+use strict;
+use warnings;
+
+use Data::Dumper;
+
+sub new {
+   my $that = shift;
+   my $proto = ref($that) || $that;
+   my $self = { @_ };
+
+   bless($self, $proto);
+
+   return $self;
+}
+
+
+1;
