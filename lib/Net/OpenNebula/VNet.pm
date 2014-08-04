ceca6da6c68456f031528f322967f6d026777853
stdweird <stijn.deweirdt@ugent.be>
2014-08-04 18:51:45 +0200

    add VNet support

--- lib/Net/OpenNebula/VNet.pm
+++ lib/Net/OpenNebula/VNet.pm
@@ -17,4 +17,29 @@ push our @ISA , qw(Net::OpenNebula::RPC);
 
 use constant ONERPC => 'vn';
 
+sub create {
+   my ($self, $tpl_txt, %option) = @_;
+   return $self->_allocate([ string => $tpl_txt ],
+                           [ int => (exists $option{cluster} ? $option{cluster} : -1) ],
+                           );
+}
+
+sub _leases {
+    my ($self, $lease_txt, $mode) = @_;
+    $mode = "add" if (! ($mode && $mode =~ m/^(add|rm)$/));
+    
+    return $self->_onerpc("${mode}leases", [ string => $lease_txt ]);
+    
+}
+
+sub addleases {
+    my ($self, $lease_txt) = @_;
+    return $self->_leases($lease_txt, "add");
+}
+
+sub rmleases {
+    my ($self, $lease_txt) = @_;
+    return $self->_leases($lease_txt, "rm");
+}
+
 1;
