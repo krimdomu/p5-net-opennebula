3f0eab3be9942e1b27285f5f72e9e8385ca6c283
stdweird <stijn.deweirdt@ugent.be>
2014-08-05 10:01:20 +0200

    add log shortcuts to main RPCClient instance, also handle passwords in request strings

--- lib/Net/OpenNebula/RPCClient.pm
+++ lib/Net/OpenNebula/RPCClient.pm
@@ -30,6 +30,13 @@ use XML::Simple;
 use RPC::XML;
 use RPC::XML::Client;
 
+
+# options
+#    user: user to connect
+#    password: password for user
+#    url: the RPC url to use
+#    log: optional log4perl-like instance
+#    fail_on_rpc_fail: die on RPC error or not
 sub new {
     my $that = shift;
     my $proto = ref($that) || $that;
@@ -39,7 +46,14 @@ sub new {
         $self->{log} = Net::OpenNebula::DummyLogger->new();
     }
 
+    # legacy behaviour
+    if (! exists($self->{fail_on_rpc_fail})) {
+        $self->{fail_on_rpc_fail} = 1;
+    }
+
     bless($self, $proto);
+
+    $self->{log}->debug(2, "Initialised with user $self->{user} and url $self->{url}.");
 
     return $self;
 }
@@ -56,12 +70,21 @@ sub _rpc {
     my $req = RPC::XML::request->new($meth, @params_o);
     my $cli = RPC::XML::Client->new($self->{url});
     
-    $self->log->debug(5, "RPC request ".$req->as_string());
+    my $reqstring = $req->as_string();
+    my $password = XMLout($self->{password}, rootname => "x");
+    if ($password =~ m!^\s*<x>(.*)</x>\s*$!) {
+        $password = quotemeta $1;
+        $reqstring =~ s/$password/PASSWORD/g;
+        $self->debug(5, "_rpc RPC request $reqstring");
+    } else {
+        $self->debug(5, "_rpc RPC request not shown, failed to convert and replace password");
+    }
+    
     my $resp = $cli->send_request($req);
     my $ret = $resp->value;
 
     if($ret->[0] == 1) {
-        $self->log->debug(5, "RPC answer $ret->[1]");
+        $self->debug(5, "_rpc RPC answer $ret->[1]");
         if($ret->[1] =~ m/^\d+$/) {
             return $ret->[1];
         }
@@ -71,10 +94,24 @@ sub _rpc {
     }   
 
     else {
-        $self->log->error("Error sending request: $ret->[1] (code $ret->[2])");
-        die("error sending request.");
+        $self->error("Error sending request: $ret->[1] (code $ret->[2])");
+        if( $self->{fail_on_rpc_fail}) {
+            die("error sending request.");
+        } else {
+            return undef;
+        }
     }   
 
 }
+
+# add logging shortcuts
+no strict 'refs';
+foreach my $i (qw(error warn info verbose debug)) {
+    *{$i} = sub {
+        my ($self, @args) = @_;
+        return $self->{log}->$i(@args);
+    }
+}
+use strict 'refs';
 
 1;
